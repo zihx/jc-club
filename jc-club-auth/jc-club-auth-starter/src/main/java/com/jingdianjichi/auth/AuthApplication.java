752ea706dd03b3c7bc55dc6d3192469cf46e6465
chao fang <939915473@qq.com>
2024-09-29 13:02:20 +0800

    nacos服务注册

--- jc-club-auth/jc-club-auth-starter/src/main/java/com/jingdianjichi/auth/AuthApplication.java
+++ jc-club-auth/jc-club-auth-starter/src/main/java/com/jingdianjichi/auth/AuthApplication.java
@@ -1,5 +1,6 @@
 package com.jingdianjichi.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.jingdianjichi")
+@MapperScan("com.jingdianjichi.**.mapper")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
